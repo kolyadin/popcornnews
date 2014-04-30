413a24ac6373c812e75b0cd6644843ac20e3c7db
Aleksey Kolyadin <donflash@gmail.com>
2014-04-30 14:53:55 +0400

    Исправлен косяк с firefox и кликом мышью по input Добавлен учет порядяка приложенных фоток

--- model/dataMaps/NewsImageDataMap.php
+++ model/dataMaps/NewsImageDataMap.php
@@ -14,10 +14,28 @@ class NewsImageDataMap extends CrossLinkedDataMap {
             $this->prepare("
                 SELECT i.* FROM pn_images AS i
                 LEFT JOIN pn_news_images AS l ON (l.imageId = i.id)
-                WHERE l.newsId = :id");
+                WHERE l.newsId = :id ORDER BY l.seq ASC");
         $this->cleanStatement = $this->prepare("DELETE FROM pn_news_images WHERE newsId = :id");
-        $this->insertStatement = $this->prepare("INSERT INTO pn_news_images (newsId, imageId) VALUES (:id, :modelId)");
+        $this->insertStatement = $this->prepare("INSERT INTO pn_news_images (newsId, imageId, seq) VALUES (:id, :modelId, :seq)");
     }
+
+	public function save($items, $id) {
+		$this->checkStatement($this->cleanStatement);
+		$this->checkStatement($this->insertStatement);
+		$this->cleanStatement->bindValue(':id', $id);
+		$this->cleanStatement->execute();
+
+		if(empty($items)) {
+			return;
+		}
+
+		foreach($items as $seq => $model) {
+			$this->insertStatement->bindValue(':id', $id);
+			$this->insertStatement->bindValue(':modelId', $model->getId());
+			$this->insertStatement->bindValue(':seq',$seq);
+			$this->insertStatement->execute();
+		}
+	}
 
     protected function mainDataMap() {
         return new ImageDataMap();
