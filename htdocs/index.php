455e44f27d931a3b8918108682769a31c8f5d321
Aleksey Kolyadin <donflash@gmail.com>
2014-06-16 23:08:59 +0400

    Изменен стиль для комментариев (like,dislike)

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,7 +5,7 @@ setlocale(LC_TIME, "ru_RU.utf8");
 require '../vendor/autoload.php';
 
 if (\popcorn\lib\Config::getMode() != 'production') {
-	xhprof_enable();
+//	xhprof_enable();
 }
 
 session_name('popcorn-session');
@@ -20,16 +20,16 @@ echo microtime(1) - $t;
 
 
 if (\popcorn\lib\Config::getMode() != 'production') {
-	$xhprof_data = xhprof_disable();
-
-	require_once '../vendor/facebook/xhprof/xhprof_lib/utils/xhprof_lib.php';
-	require_once '../vendor/facebook/xhprof/xhprof_lib/utils/xhprof_runs.php';
-
-	$namespace = 'popcorn';
-	$xhprof_runs = new XHProfRuns_Default();
-	$run_id = $xhprof_runs->save_run($xhprof_data, $namespace);
-
-	printf('<div style="margin:0 auto;padding:20px 0 30px 0;"><iframe width="100%%" height="1000" src="/xhprof_html/index.php?run=%s&source=%s"/></div>', $run_id, $namespace);
+//	$xhprof_data = xhprof_disable();
+//
+//	require_once '../vendor/facebook/xhprof/xhprof_lib/utils/xhprof_lib.php';
+//	require_once '../vendor/facebook/xhprof/xhprof_lib/utils/xhprof_runs.php';
+//
+//	$namespace = 'popcorn';
+//	$xhprof_runs = new XHProfRuns_Default();
+//	$run_id = $xhprof_runs->save_run($xhprof_data, $namespace);
+//
+//	printf('<div style="margin:0 auto;padding:20px 0 30px 0;"><iframe width="100%%" height="1000" src="/xhprof_html/index.php?run=%s&source=%s"/></div>', $run_id, $namespace);
 }
 
 function echo_arr($a) {
